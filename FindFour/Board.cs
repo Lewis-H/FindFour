e3b73e10f82bd9a1803a583bd0e0e77c87c39372
Lewis Hazell <lewis.hazell@hotmail.co.uk>
2016-11-03 22:33:36 +0000

    Removed "public" from "public get" on Moves property - typo. Removed prepended "FindFour." on exceptions, as it is not required (was added by refactoring) Added call to OnNextTurn event. Replaced "TwoDimensionalArray" namespace with new Select method, which pulls the one dimensional array between two positions in a two dimensional  array. Added x and y arguments to CheckWin method, which makes the search more efficient as only the possible four in a rows around the placed chip are searched. Removed now obsolete ProcessLine method.

--- FindFour/Board.cs
+++ FindFour/Board.cs
@@ -5,6 +5,8 @@
  */
 
 using FindFour.TwoDimensionalArray;
+using System;
+
 namespace FindFour {
     public class Board {
         private readonly int[,] positions;
@@ -27,7 +29,7 @@ namespace FindFour {
         }
 
         public int Moves { 
-            public get;
+            get;
             private set;
         }
 
@@ -48,7 +50,7 @@ namespace FindFour {
         public Board() : this(7, 6) {}
 
         public int GetChip(int x, int y) {
-            if(x >= Width || x < 0 || y >= Height || y < 0) throw new FindFour.InvalidPositionException("The position (" + x + ", " + y + ") does not exist on the board.");
+            if(x >= Width || x < 0 || y >= Height || y < 0) throw new InvalidPositionException("The position (" + x + ", " + y + ") does not exist on the board.");
             return positions[y, x];
         }
 
@@ -57,63 +59,73 @@ namespace FindFour {
         }
 
         private void PlaceChip(int x, int y) {
-            if(IsEmpty(x, y) == false) throw new FindFour.PositionTakenException("The position (" + x + ", " + y + ") is already taken by player " + GetChip(x, y) + ".");
-            if(x >= Width || x < 0 || y >= Height || y < 0) throw new FindFour.InvalidPositionException("The position (" + x + ", " + y + ") does not exist on the board.");
+            if(IsEmpty(x, y) == false) throw new PositionTakenException("The position (" + x + ", " + y + ") is already taken by player " + GetChip(x, y) + ".");
+            if(x >= Width || x < 0 || y >= Height || y < 0) throw new InvalidPositionException("The position (" + x + ", " + y + ") does not exist on the board.");
             if(OnPlace != null) OnPlace(x, y, CurrentTurn);
             positions[y, x] = CurrentTurn;
             Moves++;
-            if(!CheckWin())
-                if(Moves == Area) 
+            if(!CheckWin(x, y))
+                if(Moves == Area) {
                     if(OnDraw != null) OnDraw();
-                else
+                }else{
                     CurrentTurn = (CurrentTurn != 1) ? 1 : 2;
+                    OnNextTurn(CurrentTurn);
+                }
             else
                 if(OnWin != null) OnWin(CurrentTurn);
         }
             
         public void Drop(int column) {
-            if(column < 0 || column >= Width) throw new FindFour.InvalidColumnException("Column " + column + " does not exist on the board.");
-            if(columnPlacements[column] == Height) throw new FindFour.ColumnFullException("Column " + column + " is full!");
+            if(column < 0 || column >= Width) throw new InvalidColumnException("Column " + column + " does not exist on the board.");
+            if(columnPlacements[column] == Height) throw new ColumnFullException("Column " + column + " is full!");
             PlaceChip(column, columnPlacements[column]);
             columnPlacements[column]++;
         }
-            
-        private bool CheckWin() {
-            if(CheckRows() ||CheckColumns() || CheckDiagonals()) return true;
-            return false;
-        }
-            
-        private bool CheckRows() {
-            for(int y = 0; y < Height; y++) if(ProcessLine(Operations.GetRow<int>(positions, y))) return true;
-            return false;
-        }
-            
-        private bool CheckColumns() {
-            for(int x = 0; x < Width; x++) if(ProcessLine(Operations.GetColumn<int>(positions, x))) return true;
-            return false;
-        }
-            
-        private bool CheckDiagonals() {
-            for(int c = 3; c < Height + (Width - 4); c++) if(ProcessLine(Operations.GetDiagonal<int>(positions, DiagonalDirection.BottomRight, c))) return true;
-            for(int c = 4 - Width; c < Height - 3; c++) if(ProcessLine(Operations.GetDiagonal<int>(positions, DiagonalDirection.TopRight, c))) return true;
-            return false;
-        }
-            
-        private bool ProcessLine(int[] line) {
-            int streak = 0;
-            int previous = 0;
-            foreach(int chip in line) {
-                if(previous == 0 && previous != chip) {
-                    streak = 1;
-                }else if(previous != 0 && previous == chip) {
-                    streak++;
-                }else if(previous != chip) {
-                    streak = 1;
+
+        private bool CheckWin(int x, int y) {
+            if(x < 0 || x >= Width) throw new InvalidPositionException("x out of range.");
+            if(y < 0 || y >= Height) throw new InvalidPositionException("y out of range.");
+            int[] differences = { 3, 0, -3 };
+            int x1, y1;
+            foreach(int dx in differences) {
+                foreach(int dy in differences) {
+                    x1 = x + dx;
+                    y1 = y + dy;
+                    if(x1 >= 0 && x1 < Width && y1 >= 0 && y1 < Height && (x != x1 || y != y1)) {
+                        int[] line = Select(x, y, x1, y1);
+                        for(int i = 0; i < line.Length; i++) {
+                            if(line[i] != CurrentTurn)
+                                break;
+                            if(i == line.Length - 1)
+                                return true;
+                        }
+                    }
                 }
-                previous = chip;
-                if(streak >= 4) return true;
             }
             return false;
+        }
+
+        private int[] Select(int x0, int y0, int x1, int y1) {
+            if(!(x1 - x0 == y1 - y0)) throw new ImpossibleSelectionException("Cannot select specified line.");
+            if(x0 < 0 || x0 >= Width) throw new InvalidPositionException("x0 out of range.");
+            if(x1 < 0 || x1 >= Width) throw new InvalidPositionException("x1 out of range.");
+            if(y0 < 0 || y0 >= Height) throw new InvalidPositionException("y0 out of range.");
+            if(y1 < 0 || y1 >= Height) throw new InvalidPositionException("y1 out of range.");
+            int x = x0, y = y0;
+            int[] line = new int[Math.Max(Math.Abs(x0 - x1), Math.Abs(y0 - y1)) + 1];
+            int i = 0,
+            dx = (x0 == x1 ? 0 : x0 < x1 ? 1 : -1),
+            dy = (y0 == y1 ? 0 : y0 < y1 ? 1 : -1);
+            Func<int, int, Func<int, int, bool>> f = (n0, n1) => (n0 == n1) ? new Func<int, int, bool>((p, p1) => true) : (n0 < n1) ? new Func<int, int, bool>((p, p1) => p <= p1) : new Func<int, int, bool>((p, p1) => p >= p1);
+            Func<int, int, bool> fx = f(x0, x1);
+            Func<int, int, bool> fy = f(y0, y1);
+            while(fx(x, x1) && fy(y, y1) && i < line.Length) {
+                line[i] = positions[x, y];
+                i++;
+                x += dx;
+                y += dy;
+            }
+            return line;
         }
             
         public int[,] ToArray() {
---
+++ FindFour/ImpossibleSelectionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FindFour {
+    public class ImpossibleSelectionException : Exception {
+        public ImpossibleSelectionException() : base() {
+        }
+
+        public ImpossibleSelectionException(string message) : base(message) {
+        }
+
+        public ImpossibleSelectionException(string message, Exception inner) : base(message, inner) {
+        }
+    }
+}
+
